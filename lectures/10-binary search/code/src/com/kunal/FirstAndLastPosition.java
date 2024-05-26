91cee14a365fd1011090449fbad8ebd7803ed8ce
Sahaj <sahajguptarocks@gmail.com>
2024-05-26 11:47:46 +0530

    dd

--- lectures/10-binary search/code/src/com/kunal/FirstAndLastPosition.java
+++ lectures/10-binary search/code/src/com/kunal/FirstAndLastPosition.java
@@ -23,7 +23,7 @@ public class FirstAndLastPosition {
         int end = nums.length - 1;
         while(start <= end) {
             // find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            // int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
             int mid = start + (end - start) / 2;
 
             if (target < nums[mid]) {
